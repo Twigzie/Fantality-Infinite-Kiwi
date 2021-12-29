acb3c5e0095e15e1f2614f4048a59f86a53e2d14
Twigzie <Paradox_Slayer517@hotmail.com>
2021-12-28 20:57:27 -0600

    Initial Upload

---
+++ src/Kiwi/Kiwi/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Kiwi.Classes.Commands;
+using static Kiwi.Classes.CommandHelpers;
+using static Kiwi.Classes.ConsoleHelpers;
+
+namespace Kiwi {
+
+    internal class Program {
+
+        [STAThread]
+        public static void Main(string[] args) {
+
+            try {
+
+                Init();
+
+                while (true) {
+
+                    var command = new Command(Console.ReadLine());
+                    if (command.Valid == false)
+                        continue;
+                    else {
+                        switch (command.Name.ToLower()) {
+                            case "help":
+                                HelpCommand();
+                                continue;
+                            case "clear":
+                                ClearCommand();
+                                continue;
+                            case "about":
+                                AboutCommand();
+                                continue;
+                            case "github":
+                                GithubCommand();
+                                continue;
+                            case "browse":
+                                BrowseCommand();
+                                continue;
+                            case "update":
+                                UpdateCommand();
+                                continue;
+                            case "extract":
+                                ExtractCommand();
+                                continue;
+                            default:
+                                continue;
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception ex) {
+                Error(ex);
+            }
+            finally {
+                Close();
+            }
+
+        }
+
+        private static void Init() {
+
+            Console.Title = $"Kiwi | Font Extractor | {Environment.UserName}";
+
+            ClearCommand();
+
+        }
+
+    }
+
+}
